d719cde3e06a82705c2630a14d2d84d87f3543bb
ernestomelchor <nurturelove88@gmail.com>
2020-07-12 10:40:08 -0500

    add jwt gem, define current_user and authenticate_user methods in application_controller, test in insomnia for verification, add sessions api controller and create action

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,8 @@ Rails.application.routes.draw do
   namespace :api do
     get "/users" => "users#index"
     post "/users" => "users#create"
+    patch "/users" => "users#create"
+
+    post "/sessions" => "sessions#create"
   end
 end
