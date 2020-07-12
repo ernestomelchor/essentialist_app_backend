6ba15e38996b2eaf89ce202058368e70ab319416
ernestomelchor <nurturelove88@gmail.com>
2020-07-12 10:48:02 -0500

    begin adding more routes for users

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   namespace :api do
     get "/users" => "users#index"
     post "/users" => "users#create"
-    patch "/users" => "users#create"
+    get "/users"
 
     post "/sessions" => "sessions#create"
   end
