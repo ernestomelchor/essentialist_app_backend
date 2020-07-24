3ebb1dc6efec297b3b5cda5bf313d410da99cbe0
ernestomelchor <nurturelove88@gmail.com>
2020-07-24 15:07:13 -0500

    add validations to item and list models

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,8 @@
 class Item < ApplicationRecord
+  # validations
+  validates :description, presence: true
+
+  # associations
   belongs_to :list
   belongs_to :user
 end
--- app/models/list.rb
+++ app/models/list.rb
@@ -1,6 +1,8 @@
 class List < ApplicationRecord
+  # validations
+  validates :name, presence: true, uniqueness: true
+
   #associations
   has_many :items, dependent: :destroy
   belongs_to :user
-  validates :name, presence: true, uniqueness: true
 end
